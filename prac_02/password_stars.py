0615f73d0f8241a7038ae61240e49deb8805ee82
triciajune12 <patricia.harris@my.jcu.edu.au>
2023-09-23 15:09:51 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,19 @@
-"""Password check program"""
-password = input("Please enter password: ")
-while len(password) < 8:
-    print("Invalid password")
+def main():
+    """Password check program"""
+    password = get_password()
+    print_stars(password)
+
+
+def print_stars(password):
+    print("*" * len(password))
+
+
+def get_password():
     password = input("Please enter password: ")
-print("*" * len(password))
+    while len(password) < 8:
+        print("Invalid password")
+        password = input("Please enter password: ")
+    return password
+
+
+main()
